7693da681ee41c8c6237973e8fdfe75aa1b2adcf
Sebin Nidhiri <sebinbn@gmail.com>
2026-02-26 18:24:46 -0600

    Updated master file to generate 3_Mex_RvF_mat.png

--- MXNBnd_Replicate.R
+++ MXNBnd_Replicate.R
@@ -13,6 +13,14 @@ TAB_PATH  <- "output/tables/"
 
 source("code/0_1_setup.R")
 
-
+# Cleaning Data
 source("code/1_1_BBYieldClean.R")
 source("code/1_2_BondOwnrshpClean.R")
+source("code/1_3_EFFR_IIP_TIIE_xrateClean.R")
+
+source("code/1_4_MonthlyDataCreate.R")
+source("code/1_4_DailyDataCreate.R")
+
+# Generating Figures
+
+source("code/3_Mex_RvF_mat.R")
