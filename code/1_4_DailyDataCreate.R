d231fa0a8633e12344a958cadecc0986756c63b8
Sebin Nidhiri <sebinbn@gmail.com>
2026-03-02 12:40:46 -0600

    Instead of creating Mex_d & Mex_d_sub, only Mex_d is created as a subset of data for time period used in TVVAR. In order to use Mex_d to generate Mex_m, missing values in many variables not used in TVVAR interpolated.

--- code/1_4_DailyDataCreate.R
+++ code/1_4_DailyDataCreate.R
@@ -1,61 +1,64 @@
 # PURPOSE:
-#   1. Merge data to create daily frequency dataframe.
-#   2. Create Mex_d_sub and its first difference Mex_d_sub_diff.
+#   1. Merge data to create daily frequency dataframe Mex_d and its first
+#       difference Mex_d_diff.
 #      This is a complete weekday grid (2008-03-01 to 2022-12-31) 
 #      with key variables linearly interpolated across missing weekdays 
 #      (e.g. market holidays). This dataframe is used in TV-VAR analysis.
 #
+# Choice of sample period: 2008-04-01 to 2022-12-31
+#   - Start: MXY10Y has many missing before mid-Feb 2008;MXY01Y starts only in 
+#             01-04-2008 (April '08)
+#   - End:   MPTBA (1mo T-bill) is only available through March 2023; so ending 
+#     Dec 2022.
+#
 # INPUT:  TIIE, MXN, Yield_Data, Own_Data
-# OUTPUT: Mex_d, Mex_d_sub
+# OUTPUT: Mex_d, Mex_d_diff
 # CALLED BY: MXNBnd_Replicate.R
 
 # 1. Merging and Creating Daily Data -----------------------------------------
 
-Mex_d  = merge(Yield_Data, Own_Data[,c("Date", "F_Own", "F_Own_p")])
-Mex_d  = merge(Mex_d, MXN)
-Mex_d  = merge(Mex_d, TIIE, all= T)
-
-message("Dataframe with daily data for analysis created.")
-
-# 2. Creating Mex_d_sub and Mex_d_sub_diff - subset used in TV-VAR analysis------------
-#
-# Mex_d contains data only on dates where observations exist. For TV_VAR,
-# a complete sequence of weekdays is needed so that lag structures are 
-# meaningful (e.g. lag-5 = one trading week). Missing values
-# on market holidays are filled via linear interpolation (na.approx).
-#
-# Sample period: 2008-03-01 to 2022-12-31
-#   - Start: GMXN10Y (10yr yield) is missing before mid-Feb 2008; starting 
-#     March 2008 avoids a long leading block of NAs.
-#   - End:   MPTBA (1mo T-bill) is only available through March 2023; so ending 
-#     Dec 2022.
-
-Vars_TVVAR = c("MXY01M", "MXY03M", "MXY06M", "MXY10Y", "MXY30Y",
-             "TIIE", "MXN_USD","F_Own", "F_Own_p")
+sample_start_end = c(start = as.Date("2008-04-01"), end = as.Date("2022-12-31"))
 
 all_weekdays = data.frame(
   Date = {
-    all_days = seq.Date(from = as.Date("2008-03-01"),
-                        to   = as.Date("2022-12-31"),
+    all_days = seq.Date(from = sample_start_end["start"],
+                        to   = as.Date("2023-12-31"), 
                         by   = "day")
+    #MXY06Y and MXY08Y have missing values in 2022, but values available in 2023.
+    #To use 2023 values to interpolate, ending date is 2023. It is later subsetted to 2022.
     all_days[!weekdays(all_days) %in% c("Saturday", "Sunday")]
   }
 )
 
-Mex_d_sub = merge(all_weekdays, Mex_d[,c("Date", Vars_TVVAR)], by = "Date", all.x = TRUE)
+Mex_d  = merge(all_weekdays, Yield_Data, by = "Date", all.x = TRUE)
+Mex_d = merge(Mex_d, Own_Data[,c("Date", "F_Own", "F_Own_p")], all.x = TRUE)
+Mex_d  = merge(Mex_d, MXN, all.x = TRUE)
+Mex_d  = merge(Mex_d, TIIE[,names(TIIE) != "Tgt_rate"], all.x = TRUE)
 
 # Linearly interpolate over missing weekdays (market holidays) for key variables.
 # na.rm = FALSE preserves leading/trailing NAs rather than extrapolating.
-Mex_d_sub[Vars_TVVAR] = lapply(Mex_d_sub[Vars_TVVAR],
-                                na.approx, na.rm = FALSE)
+Vars_d_NA = names(Mex_d)[colMeans(is.na(Mex_d)) > 0]
+Mex_d[Vars_d_NA] = lapply(Mex_d[Vars_d_NA],na.approx, na.rm = FALSE)
+
+# subsetting to choose sample period
+Mex_d = Mex_d[Mex_d$Date <= sample_start_end["end"],]
+
+Vars_d_NA = names(Mex_d)[colMeans(is.na(Mex_d)) > 0]
+message(sprintf("Despite subsetting from %s to %s, %s have missing values which are linearly interpolated using na.approx()",
+                sample_start_end["start"], sample_start_end["end"],
+                paste(Vars_d_NA, collapse = ", ")
+))
+
+message("Dataframe with daily data for analysis created.")
+
 
 # Creating First differences (lapply required as diff() does not work on dataframes)
-Mex_d_sub_diff <- data.frame(
-  Date = Mex_d_sub$Date[-1],
-  lapply(Mex_d_sub[, Vars_TVVAR], diff)
+Mex_d_diff <- data.frame(
+  Date = Mex_d$Date[-1],
+  lapply(Mex_d[, Vars_TVVAR], diff)
 )
 
 # removing intermediate variables no longer necessary
-rm(all_weekdays)
+rm(all_weekdays, all_days, Vars_d_NA)
 
 message("Complete weekday dataframe with interpolated values (Mex_d_sub) and its first difference (Mex_d_sub) created.")
