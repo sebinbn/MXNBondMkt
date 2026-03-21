642af2fe99d5cbed8a3ebf84243f6540523ac485
Sebin Nidhiri <sebinbn@gmail.com>
2026-03-21 16:01:26 -0500

    Install tvReg from Github repo sebinbn/tvReg.

--- code/0_1_setup.R
+++ code/0_1_setup.R
@@ -1,5 +1,14 @@
 # Loading required packages
 
+
+if (!requireNamespace("tvReg", quietly = TRUE)) {
+  if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes")
+  }
+  library(remotes)      #for installing an R package from Github
+  remotes::install_github("sebinbn/tvReg")
+}
+
 library(readxl)       #for importing data from xlsx
 library(reshape2)     #for using melt() which converts data to long format convenient for ggplot
 library(ggplot2)      #for plots
@@ -7,5 +16,6 @@ library(zoo)          #for na.approx() used in 1_4_dailyDataCreate.R
 library(urca)         #for ur.df used in 2_1_ADFTest.R
 library(systemfit)    #for SUR estimation using systemfit()
 library(tvReg)        #for tvvar and tvirf
+
 
 message("Necessary packages loaded into enviroment")
