be3481f5d5cf2bad50e7cf07040400146c1f97db
Sebin Nidhiri <sebinbn@gmail.com>
2026-03-24 22:56:37 -0500

    Adjusting theme to adjust label sizes.

--- code/3_EME-FO.R
+++ code/3_EME-FO.R
@@ -9,6 +9,14 @@
 #
 # CALLED BY: MXNBnd_Replicate.R
 
+# Setting figure theme controls ---------------------------------------
+
+fig_theme <- theme_minimal() + theme(
+  axis.text.y = element_text(size = 17,angle = 90),
+  axis.title = element_text(size = 20),
+  axis.text.x = element_text(size = 15)
+)
+
 # 1. Plot and save the annual EME total ----------------------------------------
 
 PlotData = GDebt_FO_EM
@@ -18,22 +26,18 @@ GDebt_FO_plot <- ggplot(data = PlotData, aes(x = Year, y = Total, group = 1)) +
   geom_line(color = "darkblue", linewidth = 1.25) +
   scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
   labs(y = "Billions of $", x = NULL) +
-  theme(
-    axis.text = element_text(size = 14),
-    axis.title = element_text(size = 14),
-    axis.text.y = element_text(angle = 90)
-  )
+  fig_theme
 
 GDebt_FO_plot
 
 filename = "FOInGDebt_2004-24.png"
 ggsave(file.path(FIG_PATH, filename),  plot = GDebt_FO_plot,
-        width = 10,  height = 6,  dpi = 300)
+        width = 9,  height = 6,  dpi = 300)
 
 message(sprintf("Plot of EM's FO GovDebt over time saved in %s",
                 file.path(getwd(), FIG_PATH, filename) ))
 
-# 1. Plot and save Annual FO in GDebt as  share of GDP ---------------------
+# 2. Plot and save Annual FO in GDebt as  share of GDP ---------------------
 
 PlotData = GDebt_shares
 PlotData$Year = as.Date(paste0(PlotData$Year, "-01-01"))
@@ -41,18 +45,17 @@ PlotData$Year = as.Date(paste0(PlotData$Year, "-01-01"))
 GDebt_Share_plot <- ggplot(data = PlotData, aes(x = Year, y = Total, group = 1)) +
   geom_line(color = "forestgreen", linewidth = 1.25) +
   scale_x_date(date_breaks = "2 years", date_labels = "%Y") +
+  scale_y_continuous(
+    breaks = seq(0.06, max(PlotData$Total, na.rm = TRUE), by = 0.02)
+  ) +
   labs(y = "Share of GDP", x = NULL) +
-  theme(
-    axis.text = element_text(size = 14),
-    axis.title = element_text(size = 14),
-    axis.text.y = element_text(angle = 90)
-  )
+  fig_theme
 
 GDebt_Share_plot
 
 filename = "FOInGDebt_share_2004-24.png"
 ggsave(file.path(FIG_PATH, filename), plot = GDebt_Share_plot,
-       width = 10,  height = 6,  dpi = 300)
+       width = 9,  height = 6,  dpi = 300)
 
 message(sprintf("Plot of EM's FO GovDebt as share of GDP over time saved in %s",
                 file.path(getwd(), FIG_PATH, filename) ))
